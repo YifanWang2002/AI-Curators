4e26bd5a5e91485219dae52792aeef329b843fff
Wenqing Cao <107002041+purepisces@users.noreply.github.com>
2024-10-26 21:00:31 -0400

    modify same_artist.py

--- Recommend/channels/same_artist.py
+++ Recommend/channels/same_artist.py
@@ -35,24 +35,26 @@ class SameArtistChannel:
             unique_log = unique_log.reset_index()
         recent_artworks = unique_log.head(num_artist)["artwork_id"].tolist()
         artworks_data = self.get_artworks_for_ids(recent_artworks)
-        self.artist_ids = [artwork['artist_id'] for artwork in artworks_data]
+        artist_ids = [artwork['artist_id'] for artwork in artworks_data]
         self.candidates_list = []
-        for artist_id in self.artist_ids:
+        for artist_id in artist_ids:
             artwork_ids = self.artist_artworks.get(artist_id, [])
             random.shuffle(artwork_ids)
             self.candidates_list.append(artwork_ids)
         self.interacted_set = interacted_set
+        self.artist_ids = [f"Artist: {artist}" for artist in artist_ids]
 
     def __call__(self, recommended_set):
         unique_str_integers = [str(num) for num in random.sample(range(100), 50)]
         if not recommended_set: 
             init_tags =  ['Tag: 1'] * len(unique_str_integers)
-            print([unique_str_integers])
             return [unique_str_integers], [init_tags], len( unique_str_integers)
         exclude_set = self.interacted_set | recommended_set
         artist_recs_list = [
-            [x for x in object_ids if x not in exclude_set]
-            for object_ids in self.candidates_list
+            x for sublist in self.candidates_list for x in sublist if x not in exclude_set
         ]
-        artist_names = [f"Artist: {x}" for x in self.artist_ids]
-        return artist_recs_list, artist_names, len(artist_recs_list)
+        artist_names = []
+        for i, sublist in enumerate(self.candidates_list):
+            artist_name = self.artist_ids[i]
+            artist_names.extend([artist_name] * len([x for x in sublist if x not in exclude_set]))
+        return [artist_recs_list], [artist_names], len(artist_recs_list)
